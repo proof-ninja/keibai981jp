a3bdf4be5a4c89f117ef113228b8e299adb9082c
Yoshihiro Imai <yoshihiro503@proofcafe.org>
2022-07-29 09:35:15 +0900

    an ocaml project made using dune

---
+++ lib/main.ml
@@ -0,0 +1,6 @@
+open Lambdasoup
+
+let search () =
+  let url = "https://981.jp/ftl/searchRes.do?s.pr=12&s.mf=0&_s.rg=on&_s.pu=on&s.bp0=&s.bp1=&s.sm0=&s.sm1=&s.ag0=&s.ag1=&s.tr="
+let get_top () =
+  let url =
